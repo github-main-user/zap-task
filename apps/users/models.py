db637aa1303b59037e25a3c27c249fe265015954
github-main-user <kolya.skripn@mail.ru>
2025-07-02 15:24:55 +0300

    refactor: use abstract model for created_at and updated_at fields

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from apps.core.models import TimeStampModel
+
 from .managers import UserManager
 
 
-class User(AbstractUser):
+class User(AbstractUser, TimeStampModel):
     class UserRoles(models.TextChoices):
         CLIENT = "client", "Client"
         FREELANCER = "freelancer", "Freelancer"
@@ -14,8 +16,6 @@ class User(AbstractUser):
 
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=10, choices=UserRoles)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
