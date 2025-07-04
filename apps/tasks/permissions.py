c999d5e69321f726fc83a0d229c86cb9da3338f9
github-main-user <kolya.skripn@mail.ru>
2025-07-04 15:24:33 +0300

    feat: add proposal specific permissions

--- apps/tasks/permissions.py
+++ apps/tasks/permissions.py
@@ -4,10 +4,20 @@ from .models import Task
 
 
 class IsTaskOpen(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.status == Task.TaskStatus.OPEN
+    def has_object_permission(self, request, view, task):
+        return task.status == Task.TaskStatus.OPEN
 
 
 class IsTaskOwner(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.client == request.user
+    def has_object_permission(self, request, view, task):
+        return task.client == request.user
+
+
+class IsProposalOwner(BasePermission):
+    def has_object_permission(self, request, view, proposal):
+        return proposal.freelancer == request.user
+
+
+class IsProposalTaskOwner(BasePermission):
+    def has_object_permission(self, request, view, proposal):
+        return proposal.task.client == request.user
