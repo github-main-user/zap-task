f3a5e96ae16363e1db6ab08281081a7af24bc696
github-main-user <kolya.skripn@mail.ru>
2025-07-08 18:48:25 +0300

    test: write tests for new user-public-detail endpoint

--- apps/users/tests/test_users_views.py
+++ apps/users/tests/test_users_views.py
@@ -1,18 +1,16 @@
 import pytest
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 
-
-@pytest.fixture
-def test_user(django_user_model):
-    return django_user_model.objects.create_user("test@test.com", "pass")
+User = get_user_model()
 
 
 # token obtain pair
 
 
 @pytest.mark.django_db
-def test_obtain_token_pair_success(api_client, test_user):
+def test_obtain_token_pair_success(api_client, client_user):
     response = api_client.post(
         reverse("users:token_obtain_pair"),
         {"email": "owner@owner.com", "password": "pass"},
@@ -39,7 +37,7 @@ def test_obtain_token_pair_wrong_credentials(api_client):
 
 
 @pytest.mark.django_db
-def test_refresh_token_success(api_client, test_user):
+def test_refresh_token_success(api_client, client_user):
     response = api_client.post(
         reverse("users:token_obtain_pair"),
         {"email": "owner@owner.com", "password": "pass"},
@@ -67,78 +65,101 @@ def test_refresh_token_invalid(api_client):
 
 
 @pytest.mark.django_db
-def test_user_register_success(api_client, django_user_model):
+def client_user_register_success(api_client):
     response = api_client.post(
         reverse("users:register"), {"email": "new@user.com", "password": "pass"}
     )
 
     assert response.status_code == status.HTTP_201_CREATED
-    assert django_user_model.objects.filter(email="new@user.com").exists()
+    assert User.objects.filter(email="new@user.com").exists()
 
 
 @pytest.mark.django_db
-def test_user_register_already_exists(api_client, django_user_model, test_user):
+def client_user_register_already_exists(api_client, client_user):
     response = api_client.post(
         reverse("users:register"), {"email": "owner@owner.com", "password": "pass"}
     )
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert not django_user_model.objects.filter(email="new@user.com").exists()
+    assert not User.objects.filter(email="new@user.com").exists()
 
 
 # me/ retrieve
 
 
 def test_retrieve_me_unauthenticated(api_client):
-    response = api_client.get(reverse("users:me"))
+    response = api_client.get(reverse("users:user-me"))
 
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
 @pytest.mark.django_db
-def test_retrieve_me_success(api_client, test_user):
-    api_client.force_authenticate(test_user)
-    response = api_client.get(reverse("users:me"))
+def test_retrieve_me_success(api_client, client_user):
+    api_client.force_authenticate(client_user)
+    response = api_client.get(reverse("users:user-me"))
 
     assert response.status_code == status.HTTP_200_OK
-    assert test_user.email == response.data.get("email")
+    assert client_user.email == response.data.get("email")
 
 
 # me/ update
 
 
-def test_update_me_unauthenticated(api_client, test_user):
-    response = api_client.patch(reverse("users:me"), {"first_name": "UPDATED"})
+def test_update_me_unauthenticated(api_client, client_user):
+    response = api_client.patch(reverse("users:user-me"), {"first_name": "UPDATED"})
 
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    test_user.refresh_from_db()
-    assert test_user.first_name != "UPDATED"
+    client_user.refresh_from_db()
+    assert client_user.first_name != "UPDATED"
 
 
 @pytest.mark.django_db
-def test_update_me_success(api_client, test_user):
-    api_client.force_authenticate(test_user)
-    response = api_client.patch(reverse("users:me"), {"first_name": "UPDATED"})
+def test_update_me_success(api_client, client_user):
+    api_client.force_authenticate(client_user)
+    response = api_client.patch(reverse("users:user-me"), {"first_name": "UPDATED"})
 
     assert response.status_code == status.HTTP_200_OK
-    test_user.refresh_from_db()
-    assert test_user.first_name == "UPDATED"
+    client_user.refresh_from_db()
+    assert client_user.first_name == "UPDATED"
 
 
 # me/ delete
 
 
-def test_delete_me_unauthenticated(api_client, django_user_model, test_user):
-    response = api_client.delete(reverse("users:me"))
+def test_delete_me_unauthenticated(api_client, client_user):
+    response = api_client.delete(reverse("users:user-me"))
 
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert django_user_model.objects.filter(email=test_user.email).exists()
+    assert User.objects.filter(email=client_user.email).exists()
 
 
 @pytest.mark.django_db
-def test_delete_me_success(api_client, django_user_model, test_user):
-    api_client.force_authenticate(test_user)
-    response = api_client.delete(reverse("users:me"))
+def test_delete_me_success(api_client, client_user):
+    api_client.force_authenticate(client_user)
+    response = api_client.delete(reverse("users:user-me"))
 
     assert response.status_code == status.HTTP_204_NO_CONTENT
-    assert not django_user_model.objects.filter(email=test_user.email).exists()
+    assert not User.objects.filter(email=client_user.email).exists()
+
+
+# user public detail
+
+
+def test_retrieve_user_unauthenticated(api_client, client_user):
+    response = api_client.get(
+        reverse("users:user-public-detail", args=[client_user.pk])
+    )
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert not "id" in response.data
+
+
+@pytest.mark.django_db
+def test_retrieve_user_success(api_client, client_user, freelancer_user):
+    api_client.force_authenticate(client_user)
+    response = api_client.get(
+        reverse("users:user-public-detail", args=[freelancer_user.pk])
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data.get("id") == freelancer_user.pk
