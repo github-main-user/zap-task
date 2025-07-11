b9d452f3a9b256c772b72b420e70a17d9c938da3
github-main-user <kolya.skripn@mail.ru>
2025-07-11 11:37:01 +0300

    feat: add smpt settings

--- config/settings.py
+++ config/settings.py
@@ -152,6 +152,18 @@ REST_FRAMEWORK = {
 }
 
 
+EMAIL_BACKEND = config(
+    "EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
+)
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=False)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default=EMAIL_HOST_USER)
+
+
 SPECTACULAR_SETTINGS = {
     "TITLE": "Zap Task API",
     "DESCRIPTION": "Zap-Task is a full-featured REST API that simulates a "
