fec605b7d2d946a71a0b269fcb1852b96463c32e
github-main-user <kolya.skripn@mail.ru>
2025-07-12 16:55:50 +0300

    fix: receive ALLOWED_HOSTS from .env file

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,14 @@
 from datetime import timedelta
 from pathlib import Path
 
-from decouple import config
+from decouple import Csv, config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 
 LOGGING = {
